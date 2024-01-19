1689140fd2e9e090a2ed1c5ad6e188f62c411b81
Marc Desvaux <marc.desvaux-ext@st.com>
2024-01-19 10:38:38 +0100

    drivers: usb_dc_stm32: Fix OUT transfer issue
    The driver cannot handle OUT transactions for an endpoint with an
    MPS smaller than 64 bytes. To solve the issue, we will not use one
    fixed value, EP_MPS, but instead use the actual MPS of an endpoint,
    ep_state->ep_mps.
    
    Signed-off-by: Marc Desvaux <marc.desvaux-ext@st.com>
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- drivers/usb/device/usb_dc_stm32.c
+++ drivers/usb/device/usb_dc_stm32.c
@@ -780,7 +780,7 @@ int usb_dc_ep_enable(const uint8_t ep)
 	if (USB_EP_DIR_IS_OUT(ep) && ep != EP0_OUT) {
 		return usb_dc_ep_start_read(ep,
 					  usb_dc_stm32_state.ep_buf[USB_EP_GET_IDX(ep)],
-					  EP_MPS);
+					  ep_state->ep_mps);
 	}
 
 	return 0;
@@ -919,7 +919,7 @@ int usb_dc_ep_read_continue(uint8_t ep)
 	 */
 	if (!ep_state->read_count) {
 		usb_dc_ep_start_read(ep, usb_dc_stm32_state.ep_buf[USB_EP_GET_IDX(ep)],
-				     EP_MPS);
+				     ep_state->ep_mps);
 	}
 
 	return 0;
