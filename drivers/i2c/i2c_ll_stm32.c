4f4e0599825f3bd8140bc1787e6af153e94bf8d6
Fabrice DJIATSA <fabrice.djiatsa-ext@st.com>
2024-09-10 15:18:45 +0200

    drivers: i2c: update i2c driver to integrate stm32u0
    - enabled combined interrupt configuration for stm32u0x serie.
    - since stm32u0 serie doesn't support SMBUS we need to avoid
    use SMBHEN and SMBDEN bits register.
    
    Signed-off-by: Fabrice DJIATSA <fabrice.djiatsa-ext@st.com>

--- drivers/i2c/i2c_ll_stm32.c
+++ drivers/i2c/i2c_ll_stm32.c
@@ -79,8 +79,14 @@ int i2c_stm32_runtime_configure(const struct device *dev, uint32_t config)
 #endif
 
 	LL_I2C_Disable(i2c);
-	LL_I2C_SetMode(i2c, LL_I2C_MODE_I2C);
-	ret = stm32_i2c_configure_timing(dev, clock);
+#if defined(I2C_CR1_SMBDEN) && defined(I2C_CR1_SMBHEN)
+	i2c_stm32_set_smbus_mode(dev, data->mode);
+#endif
+	ret = stm32_i2c_configure_timing(dev, i2c_clock);
+
+	if (data->smbalert_active) {
+		LL_I2C_Enable(i2c);
+	}
 
 #ifdef CONFIG_PM_DEVICE_RUNTIME
 	(void)pm_device_runtime_put(dev);
@@ -420,6 +426,72 @@ static int i2c_stm32_pm_action(const struct device *dev, enum pm_device_action a
 }
 
 #endif
+
+#ifdef CONFIG_SMBUS_STM32_SMBALERT
+void i2c_stm32_smbalert_set_callback(const struct device *dev, i2c_stm32_smbalert_cb_func_t func,
+				     const struct device *cb_dev)
+{
+	struct i2c_stm32_data *data = dev->data;
+
+	data->smbalert_cb_func = func;
+	data->smbalert_cb_dev = cb_dev;
+}
+#endif /* CONFIG_SMBUS_STM32_SMBALERT */
+
+#if defined(I2C_CR1_SMBDEN) && defined(I2C_CR1_SMBHEN)
+void i2c_stm32_set_smbus_mode(const struct device *dev, enum i2c_stm32_mode mode)
+{
+	const struct i2c_stm32_config *cfg = dev->config;
+	struct i2c_stm32_data *data = dev->data;
+	I2C_TypeDef *i2c = cfg->i2c;
+
+	data->mode = mode;
+
+	switch (mode) {
+	case I2CSTM32MODE_I2C:
+		LL_I2C_SetMode(i2c, LL_I2C_MODE_I2C);
+		return;
+#ifdef CONFIG_SMBUS_STM32
+	case I2CSTM32MODE_SMBUSHOST:
+		LL_I2C_SetMode(i2c, LL_I2C_MODE_SMBUS_HOST);
+		return;
+	case I2CSTM32MODE_SMBUSDEVICE:
+		LL_I2C_SetMode(i2c, LL_I2C_MODE_SMBUS_DEVICE);
+		return;
+	case I2CSTM32MODE_SMBUSDEVICEARP:
+		LL_I2C_SetMode(i2c, LL_I2C_MODE_SMBUS_DEVICE_ARP);
+		return;
+#endif
+	default:
+		LOG_ERR("%s: invalid mode %i", dev->name, mode);
+		return;
+	}
+}
+#endif
+
+#ifdef CONFIG_SMBUS_STM32
+void i2c_stm32_smbalert_enable(const struct device *dev)
+{
+	struct i2c_stm32_data *data = dev->data;
+	const struct i2c_stm32_config *cfg = dev->config;
+
+	data->smbalert_active = true;
+	LL_I2C_EnableSMBusAlert(cfg->i2c);
+	LL_I2C_EnableIT_ERR(cfg->i2c);
+	LL_I2C_Enable(cfg->i2c);
+}
+
+void i2c_stm32_smbalert_disable(const struct device *dev)
+{
+	struct i2c_stm32_data *data = dev->data;
+	const struct i2c_stm32_config *cfg = dev->config;
+
+	data->smbalert_active = false;
+	LL_I2C_DisableSMBusAlert(cfg->i2c);
+	LL_I2C_DisableIT_ERR(cfg->i2c);
+	LL_I2C_Disable(cfg->i2c);
+}
+#endif /* CONFIG_SMBUS_STM32 */
 
 /* Macros for I2C instance declaration */
 
